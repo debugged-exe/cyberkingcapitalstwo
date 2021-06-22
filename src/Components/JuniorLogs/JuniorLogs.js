6f2005582fce65564ec7ed3ab53a81ecfad94921
sohamkhade <soham9623392426@gmail.com>
2021-06-22 18:36:06 +0530

    counts of junior on ffrontend

--- src/Components/JuniorLogs/JuniorLogs.js
+++ src/Components/JuniorLogs/JuniorLogs.js
@@ -20,6 +20,7 @@ import JuniorModal from "./JuniorModal/JuniorModal";
 
 // css
 import './JuniorLogs.scss';
+import {selectCurrentUser} from "../../redux/user/user.selectors";
 
 const tableLogs = [
     {
@@ -55,36 +56,66 @@ const tableLogs = [
 ]
 
 const LogStatArray = [
-    {
-        title: 'Handed Over Leads',
-        numeric: 70,
-    },
+    // {
+    //     title: 'Handed Over Leads',
+    //     numeric: 70,
+    // },
     {
         title: 'Status 1 Updated',
-        numeric: 70,
+        numeric: 0,
     },
     {
         title: 'Status 2 Updated',
-        numeric: 70,
+        numeric: 0,
     },
     {
         title: 'Unattended',
-        numeric: 70,
+        numeric: 0,
     },
     {
         title: 'Coded',
-        numeric: 70,
-    },
-    {
-        title: 'Pending Requests',
-        numeric: 70,
-    },
+        numeric: 0,
+    }
+    // {
+    //     title: 'Pending Requests',
+    //     numeric: 70,
+    // },
 ]
 
-const JuniorLogs = ({setLogStatArray, log_stat_array,setJuniorTableLogArray, setModalVisibility}) => {
+const JuniorLogs = ({currentUser, setLogStatArray, log_stat_array,setJuniorTableLogArray, setModalVisibility}) => {
 
     useEffect(() => {
-        setLogStatArray(LogStatArray);
+        const {telecaller_id} = currentUser;
+        fetch('https://aqueous-mesa-28052.herokuapp.com/junior/counts',{
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                telecaller_id: telecaller_id
+            })
+        })
+            .then(resp => resp.json())
+            .then(resp => {
+            console.log(resp);
+            LogStatArray.map(item => {
+                switch(item.title){
+                    case 'Status 1 Updated':
+                        item.numeric = resp[0].status_1;
+                        break;
+                    case 'Status 2 Updated':
+                        item.numeric = resp[0].status_2;
+                        break;
+                    case 'Unattended':
+                        item.numeric = resp[0].unattended;
+                        break;
+                    case 'Coded':
+                        item.numeric = resp[0].coded;
+                        break;
+                    default:
+                        break;
+                }
+                setLogStatArray(LogStatArray);
+            })
+        })
     }, [])
 
     const [filter, setFilter] = useState('*');
@@ -146,7 +177,8 @@ const JuniorLogs = ({setLogStatArray, log_stat_array,setJuniorTableLogArray, set
 }
 
 const mapStateToProps = createStructuredSelector({
-    log_stat_array: selectJuniorLogStatArray
+    log_stat_array: selectJuniorLogStatArray,
+    currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -7,4 +7,3 @@ export const selectCurrentUser = createSelector(
     (user) => user.currentUser
 );
 
-
