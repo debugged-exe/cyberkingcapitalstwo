6141f7a266b84898dce1963a7e160df47fbd3890
sohamkhade <55399257+sohamkhade@users.noreply.github.com>
2021-10-12 23:02:44 +0530

    send id remove functionality and pagination done

--- src/Components/AdminSendId/AdminSendId.js
+++ src/Components/AdminSendId/AdminSendId.js
@@ -1,40 +1,134 @@
 import React,{useEffect,useState} from 'react';
 import AdminSendIdTable from './AdminSendIdTable/AdminSendIdTable';
 const AdminSendId = () => {
-
+    const [language, setLanguage] = useState('');
     const[data,setData] = useState([]);
-
-
+    const [pages, setPages] = useState(0);
+    const [pageNumbers, setPageNumbers] = useState([]);
+    const perPage = 10;
     useEffect(() => {
+        setData([]);
+    }, [])
+
+    const fetchNewPage = (pgNo) => {
         fetch('https://aqueous-mesa-28052.herokuapp.com/admin/fetch_processed_removed',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                pgNo : 0,      
+                pgNo : pgNo,
+                preferred_language: language
             })
         })
-        .then( resp => resp.json())
-        .then(
-            resp=>{
-            console.log(resp)
-            setData(resp)
-            }
-        )
-        .catch(err=>console.log(err));
-    }, [])
-
-
+            .then( resp => resp.json())
+            .then(
+                resp=>{
+                    console.log(resp)
+                    setData(resp)
+                }
+            )
+            .catch(err=>console.log(err));
+    }
+    let handleChange;
+    handleChange = (event) => {
+        const {name, value} = event.target;
+        console.log(value);
+        if(value === 'hindi'){
+            setLanguage('hindi');
+            fetch('https://aqueous-mesa-28052.herokuapp.com/admin/fetch_processed_removed_page_count',{
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    preferred_language: "hindi"
+                })
+            })
+                .then( resp => resp.json())
+                .then(
+                    resp=>{
+                        console.log(resp);
+                        setPages(resp.count);
+                        var arr = [];
+                        for (let i = 1; i <= resp.count; i++) {
+                            arr.push(i);
+                        }
+                        setPageNumbers(arr);
+                    }
+                )
+                .catch(err=>console.log(err));
+        fetch('https://aqueous-mesa-28052.herokuapp.com/admin/fetch_processed_removed',{
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                pgNo : 0,
+                preferred_language: "hindi"
+            })
+        })
+            .then( resp => resp.json())
+            .then(
+                resp=>{
+                    console.log(resp)
+                    setData(resp)
+                }
+            )
+            .catch(err=>console.log(err));
+        }
+        if(value==='marathi'){
+            setLanguage('marathi');
+            fetch('https://aqueous-mesa-28052.herokuapp.com/admin/fetch_processed_removed_page_count',{
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    preferred_language: "marathi"
+                })
+            })
+                .then( resp => resp.json())
+                .then(
+                    resp=>{
+                        console.log(resp);
+                        setPages(resp.count);
+                        var arr = [];
+                        for (let i = 1; i <= resp.count; i++) {
+                            arr.push(i);
+                        }
+                        setPageNumbers(arr);
+                    }
+                )
+                .catch(err=>console.log(err));
+            fetch('https://aqueous-mesa-28052.herokuapp.com/admin/fetch_processed_removed',{
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    pgNo : 0,
+                    preferred_language: "marathi"
+                })
+            })
+                .then( resp => resp.json())
+                .then(
+                    resp=>{
+                        console.log(resp)
+                        setData(resp)
+                    }
+                )
+                .catch(err=>console.log(err));
+        }
+    }
     return (
         <div className='mt2'>
             <div className={'flex justify-center items-center center mb4 f2 w-100 mt4'}>
             <label className={'b mr3'}>Select Language: </label>
-            <select name="lang" className={'f3 ml1'}>
+            <select name="lang" className={'f3 ml1'} onChange={handleChange}>
                 <option value="*" >--select--</option>
                 <option value="hindi">Hindi</option>
                 <option value="marathi">Marathi</option>
             </select>
         </div>
-            <AdminSendIdTable data={data}/>
+            <AdminSendIdTable data={data} language={language} setData={setData}/>
+            <div className="junior-log-pagination-container pb4">
+                <p>. . </p>
+                {pageNumbers.map((number, index) => (
+                    <button key={index} onClick={() => fetchNewPage(number-1)} className="junior-log-page-btn">{number}</button>
+                ))}
+                <p>. . </p>
+            </div>
         </div>
     )
 }
