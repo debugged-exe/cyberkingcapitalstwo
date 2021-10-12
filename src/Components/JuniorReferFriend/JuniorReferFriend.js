793a9d766a9e7238382de7bf65e9b7baef77a8e4
tejasjokin <61027009+tejasjokin@users.noreply.github.com>
2021-10-12 10:11:57 +0530

    Disable referral form

--- src/Components/JuniorReferFriend/JuniorReferFriend.js
+++ src/Components/JuniorReferFriend/JuniorReferFriend.js
@@ -37,67 +37,67 @@ class JuniorReferFriend extends Component{
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setVisible(true);
-        const {
-            lead_name,
-            lead_contact, 
-            city, 
-            prior_knowledge, 
-            preferred_language, 
-            course_type, 
-            assigned_to
-        } = this.state;
-        fetch('https://aqueous-mesa-28052.herokuapp.com/basicform', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                lead_name: lead_name,
-                lead_contact: lead_contact,
-                city: city,
-                preferred_language: preferred_language,
-                prior_knowledge: prior_knowledge,
-                course_type: course_type,
-                assigned_to: assigned_to
-            })
-        })
-        .then(response => response.json())
-        .then(response => {
-            if(response === "Basic Course Form Registered"){
-                this.setVisible(false);
-                toast.success('Registered Successfully.',{
-                    position: toast.POSITION.TOP_CENTER,
-                    autoClose: 2500
-                })
-                this.setState({
-                    lead_name: '',
-                    lead_contact: '',
-                    city: '',
-                    preferred_language: '',
-                    prior_knowledge: '',
-                    course_type: 'basic'
-                })
-            }else if(response === "Failed"){
-                this.setVisible(false);
-                toast.error("Failed to register for the course. Please try again",{
-                    position: toast.POSITION.TOP_CENTER,
-                    autoClose: 2500
-                });
-            }else if(response === "Not Unique"){
-                this.setVisible(false);
-                toast.error("Contact has been already registered.Please try with new contact",{
-                    position: toast.POSITION.TOP_CENTER,
-                    autoClose: 2500
-                });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            this.setVisible(false);
-            toast.error(`${err}`,{
-                position: toast.POSITION.TOP_CENTER,
-                autoClose: 2500
-            });
-        })
+        // this.setVisible(true);
+        // const {
+        //     lead_name,
+        //     lead_contact, 
+        //     city, 
+        //     prior_knowledge, 
+        //     preferred_language, 
+        //     course_type, 
+        //     assigned_to
+        // } = this.state;
+        // fetch('https://aqueous-mesa-28052.herokuapp.com/basicform', {
+        //     method: 'post',
+        //     headers: {'Content-Type': 'application/json'},
+        //     body: JSON.stringify({
+        //         lead_name: lead_name,
+        //         lead_contact: lead_contact,
+        //         city: city,
+        //         preferred_language: preferred_language,
+        //         prior_knowledge: prior_knowledge,
+        //         course_type: course_type,
+        //         assigned_to: assigned_to
+        //     })
+        // })
+        // .then(response => response.json())
+        // .then(response => {
+        //     if(response === "Basic Course Form Registered"){
+        //         this.setVisible(false);
+        //         toast.success('Registered Successfully.',{
+        //             position: toast.POSITION.TOP_CENTER,
+        //             autoClose: 2500
+        //         })
+        //         this.setState({
+        //             lead_name: '',
+        //             lead_contact: '',
+        //             city: '',
+        //             preferred_language: '',
+        //             prior_knowledge: '',
+        //             course_type: 'basic'
+        //         })
+        //     }else if(response === "Failed"){
+        //         this.setVisible(false);
+        //         toast.error("Failed to register for the course. Please try again",{
+        //             position: toast.POSITION.TOP_CENTER,
+        //             autoClose: 2500
+        //         });
+        //     }else if(response === "Not Unique"){
+        //         this.setVisible(false);
+        //         toast.error("Contact has been already registered.Please try with new contact",{
+        //             position: toast.POSITION.TOP_CENTER,
+        //             autoClose: 2500
+        //         });
+        //     }
+        // })
+        // .catch(err => {
+        //     console.log(err);
+        //     this.setVisible(false);
+        //     toast.error(`${err}`,{
+        //         position: toast.POSITION.TOP_CENTER,
+        //         autoClose: 2500
+        //     });
+        // })
     }
 
     handleChange = event => {
