cb84ab5d68b73f558f4ad67bf010e703ccd6988b
Swati Mandal <Swatimandal01@gmail.com>
2022-04-06 18:54:35 +0530

    Ui

--- src/Components/JuniorBox/JuniorBox.js
+++ src/Components/JuniorBox/JuniorBox.js
@@ -1,30 +1,58 @@
-import React,{useState} from "react";
-import "./JuniorBox.scss";
+import React, { useState } from "react";
+import "./JuniorBox.css";
 
-const JuniorBox = () =>{
-    const [newFetchVisibility,setNewFetchVisibility]=useState(true);
+const JuniorBox = () => {
+  const [newFetchVisibility, setNewFetchVisibility] = useState(true);
 
-    const [table,setTable]=useState('0');
-    
-    const onClick=(event)=>{
-        setTable(event.target.id);
-    }
-    
-    return(
-        <>
-        <div style={{width: "100%", height: "100%",padding:'1em',zIndex:'11'}}>
-            <div style={{width: "100%",padding:'1em',display:'flex',justifyContent:'center',alignItems:'center'}}>
-                <button >New Fetch</button>
-            </div>
-            <div className="buttons" style={{display: "flex", width:"100%",padding:'1em', justifyContent: "space-around"}}>
-                <button onClick={onClick} id='0'>A</button>
-                <button onClick={onClick} id='1'>B</button>
-                <button onClick={onClick} id='2'>C</button>
-            </div>
-            {table}
+  const [table, setTable] = useState("0");
+
+  const onClick = (event) => {
+    setTable(event.target.id);
+    console.log(event.target.id);
+  };
+
+  return (
+    <>
+      <div className="JuniorBox">
+        <div
+          style={{
+            width: "100%",
+            padding: "1em",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <button className="btn">New Fetch</button>
         </div>
-        </>
-    );
-}
+        <div
+          className="buttons"
+          style={{
+            display: "flex",
+            width: "100%",
+            padding: "1em",
+            justifyContent: "space-around",
+          }}
+        >
+          <button className="btn" onClick={onClick} id="0">
+            A
+          </button>
+          <button className="btn" onClick={onClick} id="1">
+            B
+          </button>
+          <button className="btn" onClick={onClick} id="2">
+            C
+          </button>
+        </div>
+
+        <div className="junior-tables">
+          <div className="table-1 table-box">
+            <label className="boxName">{table}</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default JuniorBox;
